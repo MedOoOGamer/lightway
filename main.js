1d11812df2a8e1b3ee3445608733820d5dc3f753
MedOoOGamer <42717674+MedOoOGamer@users.noreply.github.com>
2021-01-27 01:20:55 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -11,59 +11,20 @@ client.events = new Discord.Collection();
     require(`./handlers/${handler}`)(client, Discord);
 })
 
- 
-// const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-// for(const file of commandFiles){
-//     const command = require(`./commands/${file}`);
- 
-//     client.commands.set(command.name, command);
-// }
- 
- 
-// client.once('ready', () => {
-//     console.log('Med is online !');
-//     memberCounter(client);
-// });
 
 
 client.on('guildMemberAdd', guildMember =>{
-     let welcomeRole = guildMember.guild.roles.cache.find(role => role.name === 'Light Members');
+    let welcomeRole = guildMember.guild.roles.cache.find(role => role.name === 'Light Members');
  
-     guildMember.roles.add(welcomeRole);/     guildMember.guild.channels.cache.get('802477687620894720').send(`Welcome <@${guildMember.user.id}> to Light Way server have fun and chill`)
- });
-
- 
-// client.on('message', message =>{
-//     if(!message.content.startsWith(prefix) || message.author.bot) return;
- 
-//     const args = message.content.slice(prefix.length).split(/ +/);
-//     const command = args.shift().toLowerCase();
- 
-    
-//     if(command === 'help'){
-//         client.commands.get('help').execute(message, args, Discord);
-//     } else if(command === 'kick'){
-//         client.commands.get('kick').execute(message, args);
-//     } else if(command === 'clear'){
-//         client.commands.get('clear').execute(message, args);
-//     } else if(command === 'ban'){
-//         client.commands.get('ban').execute(message, args);
-//     } else if(command === 'mute'){
-//         client.commands.get('mute').execute(message, args);
-//     } else if(command === 'unmute'){
-//         client.commands.get('unmute').execute(message, args);
-//     } else if(command === 'play'){
-//         client.commands.get('play').execute(message, args);
-//     } else if(command === 'leave'){
-//         client.commands.get('leave').execute(message, args);
-//     } 
-// });
+    guildMember.roles.add(welcomeRole);
+    guildMember.guild.channels.cache.get('802477687620894720').send(`Welcome <@${guildMember.user.id}> to Light Way server have fun and chill`)
+});
 
 
 
 
-//client.login('ODAyMDcyMzM5ODQ3MzE1NDg3.YAp6Ew.csmu2Kof6NdZAZzTMSHxUn9NUGE');
+client.login('ODAyMDcyMzM5ODQ3MzE1NDg3.YAp6Ew.ptzfn_KFVCt8m4ANszWN9ceDK8w');
 
-client.login(process.env.token);
+//client.login(process.env.token);
 
 
